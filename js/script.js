3d66bf739778e82eca3fc63c9377ee3081fef9a4
moelatt <moelatt.cs@gmail.com>
2019-08-21 16:55:55 -0700

    Updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+const navItemI = document.querySelectorAll('.nav-itemI')
+const navItemA = document.querySelectorAll('.nav-itemA')
 function onClickMenu(){
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
+    navItemI.forEach(item => item.classList.toggle('show'));
+}
+function onClickAbout(){
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("about-bg").classList.toggle("changeAbout-bg");
+    navItemA.forEach(item => item.classList.toggle('aboutShow'));
 }
